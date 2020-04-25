e44350e80ea3764a02e77809f176fbfa2a4b5bf5
qiao0726 <51840783+qiao0726@users.noreply.github.com>
2020-04-25 17:33:42 +0800

    Initial Commit

---
+++ XcodeFirstProject/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  XcodeFirstProject
+//
+//  Created by 谯楠 on 2020/4/25.
+//  Copyright © 2020 谯楠. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
